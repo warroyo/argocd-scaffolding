a7e186d560e7c2100f997386b888e0ef520108b0
warroyo <warroyo7199008@gmail.com>
2026-01-29 11:47:01 -0500

    tf work

--- terraform/modules/argocd-attach-sv-namespace/provider.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-     k8sconnect = {
-      source = "jmorris0x0/k8sconnect"
-      version = "0.3.6"
-    }
-
-  }
-}
--- terraform/ops-tenant/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "argo_ip" {
-  value = module.argocd-instance.server_ip
-}
-
-output "argo_password" {
-  value = module.argocd-instance.admin_password
-  sensitive = true
-}
